86c38173b6516aa9619a9441066c24061112a615
Abhishek Bhardwaj <abshkbh@gmail.com>
2020-05-17 00:51:13 -0700

    Basic parsing working

--- keepass-to-lastpass.py
+++ keepass-to-lastpass.py
@@ -1,5 +1,18 @@
+import csv
+import sys
+
+
 def main():
     print("Hello World!")
+    if len(sys.argv) < 1:
+        print("keepass-to-lastpass <input-csv>")
+        return
+
+    with open(sys.argv[1]) as csvfile:
+        rows = csv.reader(csvfile, delimiter=',')
+        for row in rows:
+            if "," in row[3]:
+                print(row)
 
 
 if __name__ == "__main__":
